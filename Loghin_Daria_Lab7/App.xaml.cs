97238442ef3c4d7d4a217337e431dd003a2f7186
darialoghin <daria.loghin139@yahoo.com>
2022-11-27 00:33:29 +0200

    Add project files.

---
+++ Loghin_Daria_Lab7/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Loghin_Daria_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Loghin_Daria_Lab7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Loghin_Daria_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
