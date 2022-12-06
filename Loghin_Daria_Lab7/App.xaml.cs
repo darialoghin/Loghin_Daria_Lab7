26236aa35b0d9013748f5674b8fdb650e2e732df
darialoghin <daria.loghin139@yahoo.com>
2022-12-06 15:51:26 +0200

    Lab8

--- Loghin_Daria_Lab7/App.xaml.cs
+++ Loghin_Daria_Lab7/App.xaml.cs
@@ -1,11 +1,29 @@
-﻿namespace Loghin_Daria_Lab7;
+﻿using System;
+using Loghin_Daria_Lab7.Data;
+using System.IO;
+
+namespace Loghin_Daria_Lab7;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
 
-		MainPage = new AppShell();
-	}
 }
